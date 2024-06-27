9fca0139ffb20b52df33c404b7173c79e0ce773e
Paul Christianson <pacdeveloping@gmail.com>
2024-06-27 15:10:57 -0400

    Book object, add to library, and display library functions working

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ const myLibrary = [];
 function Book(title,author,length,readStatus) {
   this.title = title;
   this.author = author;
-  this.readStatus = readStatus;
   this.length = length;
+  this.readStatus = readStatus;
 }
 
 function addBookToLibrary(title,author,length,readStatus) {
-    newBook = new Book(title,author,length,readStatus);
+    const newBook = new Book(title,author,length,readStatus);
     myLibrary.push(newBook)
 }
+
+function displayMyLibrary() {
+    let bookTable = document.querySelector('table');
+    for (let i=0;i<myLibrary.length;i++) {
+        const newRow = document.createElement('tr');
+        ['title','author','length','readStatus'].forEach(key => {
+            const cell = document.createElement('td');
+            cell.textContent = myLibrary[i][key];
+            newRow.appendChild(cell)
+        })
+        bookTable.appendChild(newRow)
+    }
+    
+}
+
+addBookToLibrary('Illiad','Homer','968','No');
+addBookToLibrary('Moby-Dick','Herman Melville','635','Yes')
+displayMyLibrary();
+
