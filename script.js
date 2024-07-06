518befa49d1ae391bceb37fec4192b062be89b87
Paul Christianson <pacdeveloping@gmail.com>
2024-07-05 23:45:53 -0400

    Set up form with button that adds a book to the library, and updates the table to display it

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function addBookToLibrary(title,author,length,readStatus) {
 
 function displayMyLibrary() {
     let bookTable = document.querySelector('table');
+    let bookHeader = document.querySelector('.headers')
+    bookTable.replaceChildren(bookHeader)
     for (let i=0;i<myLibrary.length;i++) {
         const newRow = document.createElement('tr');
         ['title','author','length','readStatus'].forEach(key => {
@@ -30,10 +32,30 @@ const addBook = document.querySelector('#add');
 const bookForm = document.querySelector('form');
 
 addBook.addEventListener('click', () => {
-  bookForm.classList.remove('hidden');
-});
+  bookForm.classList.remove('hidden')
+})
 
-addBookToLibrary('Illiad','Homer','968','No');
+const confirmBook = document.querySelector('#submit');
+let newBookForm = document.getElementById('newBookForm');
+
+newBookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let title = document.getElementById('title');
+    let author = document.getElementById('author');
+    let length = document.getElementById('pages');
+    let readStatus = document.getElementById('readStatus');
+    if (title.value == "" || author.value == "" || length.value == "") { //would be nice to add a check that length is a nonzero integer
+        //return some sort of error message to the user
+    }
+    else {
+        if (readStatus.checked == false){readStatus.value = 'No';}
+        else {readStatus.value = 'Yes';}
+        addBookToLibrary(title.value,author.value,length.value,readStatus.value)
+        displayMyLibrary()
+    }
+})
+
+addBookToLibrary('Illiad','Homer','968','No')
 addBookToLibrary('Moby-Dick','Herman Melville','635','Yes')
-displayMyLibrary();
+displayMyLibrary()
 
